de91ba56205b68265f6d0251763d41686bff919f
James Moore <jamesm0025@gmail.com>
2018-04-01 15:00:42 -0400

    added events controller and model

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,7 +37,7 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
                             <li><a class="nav-link" href="#">Profile</a></li>
-                            <li><a class="nav-link" href="#">Event</a></li>
+                            <li><a class="nav-link" href="event">Event</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
